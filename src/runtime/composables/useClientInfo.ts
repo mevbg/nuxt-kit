9b4247622d49db1ba3a854c963bd288f54b2b23d
Martin Metodiev <m@mev.bg>
2025-08-14 19:39:02 +0300

    Fix reactivity of useClientInfo

--- src/runtime/composables/useClientInfo.ts
+++ src/runtime/composables/useClientInfo.ts
@@ -1,22 +1,24 @@
 import { computed, onBeforeMount, ref } from 'vue';
 
+const data = ref<Record<string, any>>({});
+
+const classes = computed(() =>
+  Object.keys(data.value).filter(
+    (key) => typeof (data.value as any)[key] === 'boolean' && (data.value as any)[key]
+  )
+);
+
 export function useClientInfo() {
-  const data = ref<Record<string, any>>({});
-
-  const classes = computed(() =>
-    Object.keys(data.value).filter(
-      (key) => typeof (data.value as any)[key] === 'boolean' && (data.value as any)[key]
-    )
-  );
-
-  onBeforeMount(async () => {
-    const {
-      default: defaultInfo,
-      deviceDetect,
-      ...clientData
-    } = await import('mobile-device-detect');
-    data.value = clientData;
-  });
+  if (import.meta.client) {
+    onBeforeMount(async () => {
+      const {
+        default: defaultInfo,
+        deviceDetect,
+        ...clientData
+      } = await import('mobile-device-detect');
+      data.value = clientData;
+    });
+  }
 
   return { data, classes };
 }
