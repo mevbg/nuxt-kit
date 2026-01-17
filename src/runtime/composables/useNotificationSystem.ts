e308bc04c1ebafd76b4829ba2cc7b2403e074e41
Martin Metodiev <martin@mev.bg>
2026-01-17 16:27:49 +0200

    Refine useNotificationSystem to return toasts

--- src/runtime/composables/useNotificationSystem.ts
+++ src/runtime/composables/useNotificationSystem.ts
@@ -2,21 +2,17 @@ import type { ToastContainerOptions } from 'vue3-toastify';
 import { toast } from 'vue3-toastify';
 
 export function useNotificationSystem() {
-  const notifyInfo = (label: string, config: ToastContainerOptions = {}) => {
+  const notifyInfo = (label: string, config: ToastContainerOptions = {}) =>
     toast(label, { type: 'info', ...config });
-  };
 
-  const notifySuccess = (label: string, config: ToastContainerOptions = {}) => {
+  const notifySuccess = (label: string, config: ToastContainerOptions = {}) =>
     toast(label, { type: 'success', ...config });
-  };
 
-  const notifyWarning = (label: string, config: ToastContainerOptions = {}) => {
+  const notifyWarning = (label: string, config: ToastContainerOptions = {}) =>
     toast(label, { type: 'warning', ...config });
-  };
 
-  const notifyError = (label: string, config: ToastContainerOptions = {}) => {
+  const notifyError = (label: string, config: ToastContainerOptions = {}) =>
     toast(label, { type: 'error', ...config });
-  };
 
   return {
     notifyInfo,
